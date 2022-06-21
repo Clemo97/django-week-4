bfd555472af3df480546bfcf03f31ede070e0889
Clemo97 <lumumbaclement@gmail.com>
2022-06-21 08:47:43 +0300

    user update class model

--- neighbor/forms.py
+++ neighbor/forms.py
@@ -11,3 +11,10 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username','email','password1','password2')
 
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email']
